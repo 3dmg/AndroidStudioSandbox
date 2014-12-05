644483150c8de009a1954bc6a2444eb87de6004f
Markus <dreidmg@gmail.com>
2014-12-05 16:13:03 +0100

    lollipop design tests

--- mobile/src/main/java/at/mg/androidstudiosandbox/PlaceholderFragment.java
+++ mobile/src/main/java/at/mg/androidstudiosandbox/PlaceholderFragment.java
@@ -1,13 +1,18 @@
 package at.mg.androidstudiosandbox;
 
+import android.animation.Animator;
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -40,11 +45,40 @@ public class PlaceholderFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        final TextView textView = (TextView) rootView.findViewById(R.id.home_text);
+        //set the elevation in code; in pixel
+        ViewCompat.setElevation(textView, 40);
+//        textView.setElevation(50);
+
+        TextView newtext = new TextView(getActivity());
+        newtext.setText("FLY IN");
+
+        textView.post(new Runnable() {
+            @Override
+            public void run() {
+                Animator reveal = ViewAnimationUtils.createCircularReveal(
+                        textView, // The new View to reveal
+                        0,      // x co-ordinate to start the mask from
+                        0,      // y co-ordinate to start the mask from
+                        0,  // radius of the starting mask
+                        180);   // radius of the final mask
+                reveal.start();
+            }
+        });
+
+
+        final View image = rootView.findViewById(R.id.home_image);
+
         Button button = (Button) rootView.findViewById(R.id.home_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), ToolbarActivity.class));
+                Intent intent = new Intent(getActivity(), ToolbarActivity.class);
+                String transitionName = getString(R.string.transition_image);
+//                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), image, transitionName);
+//                ActivityCompat.startActivity(getActivity(), intent, optionsCompat);
+                ActivityOptions optionsCompat = ActivityOptions.makeSceneTransitionAnimation(getActivity(), image, transitionName);
+                startActivity(intent, optionsCompat.toBundle());
             }
         });
 
@@ -57,4 +91,6 @@ public class PlaceholderFragment extends Fragment {
         ((HomeActivity) activity).onSectionAttached(
                 getArguments().getInt(ARG_SECTION_NUMBER));
     }
+
+
 }
