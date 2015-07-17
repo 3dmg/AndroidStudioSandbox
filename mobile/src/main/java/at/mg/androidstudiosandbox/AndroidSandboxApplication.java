cde7f707890dec87ef85483c3942f7f5c29502a7
Markus <dreidmg@gmail.com>
2015-07-17 17:07:02 +0200

    set apk name add Stetho

---
+++ mobile/src/main/java/at/mg/androidstudiosandbox/AndroidSandboxApplication.java
@@ -0,0 +1,31 @@
+package at.mg.androidstudiosandbox;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Application class :)
+ * <p/>
+ * Created by Markus on 17.07.2015.
+ */
+public class AndroidSandboxApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            // http://facebook.github.io/stetho/
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(
+                                    Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(
+                                    Stetho.defaultInspectorModulesProvider(this))
+                            .build());
+        }
+    }
+
+}
+
