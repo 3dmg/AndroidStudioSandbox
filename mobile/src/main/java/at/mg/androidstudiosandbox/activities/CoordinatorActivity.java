79255070d84cc38513febd427c63c43e11fe488b
Markus <dreidmg@gmail.com>
2015-11-18 11:26:43 +0100

    toolbar fixes

--- mobile/src/main/java/at/mg/androidstudiosandbox/activities/CoordinatorActivity.java
+++ mobile/src/main/java/at/mg/androidstudiosandbox/activities/CoordinatorActivity.java
@@ -3,14 +3,21 @@ package at.mg.androidstudiosandbox.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 
 import at.mg.androidstudiosandbox.R;
+
 
 public class CoordinatorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TypedValue typedValue = new TypedValue();
+        this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
+        final int color = typedValue.data;
+
+        getWindow().setStatusBarColor(color);
         setContentView(R.layout.activity_coordinator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
